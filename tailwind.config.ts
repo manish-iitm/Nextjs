1497bed021bfc3a5add3d2f170fe1c97ec33ab69
Manish Patil <pmanish332007@gmail.com>
2025-07-30 06:41:47 +0000

    Make Font to https://fonts.googleapis.com/css2?family=Montserrat+Alterna

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        body: ['"Montserrat Alternates"', 'sans-serif'],
+        headline: ['"Montserrat Alternates"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
