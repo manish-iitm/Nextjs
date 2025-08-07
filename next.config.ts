67aba60e05fec672148348736f1d144853179b29
Your Name <pmanish332007@gmail.com>
2025-08-07 08:04:14 +0000

    fix: update build and deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/Siv0',
   typescript: {
     ignoreBuildErrors: true,
   },
