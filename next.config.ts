ad1c3fce3d7e7e44a666f5ba16d4c89c27d4c2af
Your Name <pmanish332007@gmail.com>
2025-08-07 07:41:59 +0000

    feat: deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/Siv0',
   typescript: {
     ignoreBuildErrors: true,
   },
